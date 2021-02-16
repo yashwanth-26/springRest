70387b80d3f2e898a8968030c56d03a5cc5f2141
yashwanth-26 <54655172+yashwanth-26@users.noreply.github.com>
2021-02-16 20:52:56 +0530

    Add files via upload

---
+++ assign3/src/main/java/com/yashwanth/mvc/rest/AssignService.java
@@ -0,0 +1,23 @@
+package com.yashwanth.mvc.rest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+//import com.yashwanth.topic.Topics;
+
+@Service
+public class AssignService {
+	public List<AssignModel> assign = new ArrayList<>( Arrays.asList(
+			new AssignModel("AK","ANCHORAGE","US","99501")
+			
+			));
+	public AssignModel getDetails(String id)
+	{
+	return	assign.stream().filter(t -> t.getPin().equals(id)).findFirst().get();	
+	}
+
+}
