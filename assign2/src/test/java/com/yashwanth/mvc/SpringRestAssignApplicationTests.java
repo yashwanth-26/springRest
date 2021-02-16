c86c19bd023ee2419bed9cacea6a5d1f15a41c0d
yashwanth-26 <54655172+yashwanth-26@users.noreply.github.com>
2021-02-16 20:50:38 +0530

    Add files via upload

---
+++ assign2/src/test/java/com/yashwanth/mvc/SpringRestAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashwanth.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
