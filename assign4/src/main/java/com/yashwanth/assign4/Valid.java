335777769b96ca3eb1138a3a5475dce061fe917b
yashwanth-26 <54655172+yashwanth-26@users.noreply.github.com>
2021-02-16 20:54:29 +0530

    Add files via upload

---
+++ assign4/src/main/java/com/yashwanth/assign4/Valid.java
@@ -0,0 +1,19 @@
+package com.yashwanth.assign4;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Valid {
+	
+		private String aString;
+
+		public String getaString() {
+			return aString;
+		}
+
+		public void setaString(String aString) {
+			this.aString = aString;
+		}
+
+}
+
+
